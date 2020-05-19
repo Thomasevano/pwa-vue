a6ad044f0c94b0f24699b701fd980fba634e7629
Thomas Evano <thomasevano@MacBook-Pro-de-Thomas.local>
2020-05-19 16:31:50 +0200

    -add latest articles on Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="home">
-    <div v-for="post in posts" :key="post.id" class="last-article">
-      <figure>
-        <img
-              v-if="post.img"
-              :src="post.img"
-              alt=""
-            >
-            <img
-              v-else
-              src="http://via.placeholder.com/250x250"
-              alt=""
-            >
-      </figure>
-      <span>Jean claude</span>
-      <span>Le 14/04/2020</span>
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.description}}</p>
+      <div v-for="post in latestPosts" :key="post.id" class="lastest-articles">
+        <router-link :to="'/article/' + post.id">
+          <figure>
+            <img v-if="post.img" :src="post.img" alt="article image">
+            <img v-else src="http://via.placeholder.com/250x250" alt="article image">
+          </figure>
+          <span>{{ post.author }}</span>
+          <span>{{ post.date }}</span>
+          <h2>{{ post.title }}</h2>
+          <p>{{ post.description}}</p>
+        </router-link>
+      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -26,6 +20,13 @@
 
 export default {
   name: 'Home',
+  computed: {
+    latestPosts: function () {
+      return this.posts.filter(function (post) {
+        return post.id < 5 & post.id > 1
+      })
+    }
+  },
   created() {
     fetch('https://tp-vuepwa.glitch.me/').then((response) => {
       response.json().then((data) => {
